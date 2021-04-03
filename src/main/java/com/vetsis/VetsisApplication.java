2a2bac088d03d1ddcde897fd7969baee216892d2
Nicolas Ferretti <nicolas.ferretti@rappi.com>
2021-04-02 23:10:36 -0300

    first commit

---
+++ src/main/java/com/vetsis/VetsisApplication.java
@@ -0,0 +1,13 @@
+package com.vetsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetsisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetsis/VetsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
